8784443bfca3367ce9b58741129f219a034d9d87
GameProg <>
2016-10-26 18:57:53 +0200

    pong

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    private Rigidbody rb = null;
+
+    [SerializeField]
+    private int force = 500;
+
+    [SerializeField]
+    private Vector3 scale = new Vector3(1, 1, 1);
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        int random = Random.Range(0, 2);
+
+        if (random == 1)
+            rb.AddForce(force, 0, 0);
+        else
+            rb.AddForce(-force, 0, 0);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ItemBigger")
+        {
+            Destroy(collision.gameObject);
+            transform.localScale += scale;
+            StartCoroutine(scaleDelay());
+        }
+
+        if (collision.collider.tag == "ItemNewBall")
+        {
+            Instantiate(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private IEnumerator scaleDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        transform.localScale -= scale;
+    }
+}
---
+++ Assets/ItemGetBigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ItemGetBigger : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 scale = new Vector3(1, 1, 1);
+
+    //  private float timeDelay = 3;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ball")
+        {
+            collision.transform.localScale += scale;
+
+            StartCoroutine(delay(collision));
+
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    IEnumerator delay(Collision collision)
+    {
+        yield return new WaitForSeconds(3f);
+
+        collision.transform.localScale -= scale;
+        Destroy(gameObject);
+    }
+
+}
